f5c63018f10ecdb8ff10b07f3018ab39b1cc06c4
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-18 01:23:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,14 @@
 
 
-def prime?(n)
-  if(n<2)
-    return false
-  end
-   else(2..n-1).none? {|divisor| n % divisor == 0}
-end
+
+Failures:
+
+  1) prime? returns false for non-prime numbers
+     Failure/Error: expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
+       Be sure to account for negative numbers!
+     # ./spec/prime_spec.rb:13:in `block (2 levels) in <top (required)>'
+
+Finished in 0.02539 seconds (files took 0.11481 seconds to load)
+2 examples, 1 failure
+
+Failed examples:
